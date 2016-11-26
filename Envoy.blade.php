da16d613674c20d35d0b1efe5ac54878ac662f47
Piyush Agrawal <poush12@gmail.com>
2016-11-26 11:21:48 +0530

    welcome page

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,1 +1,6 @@
 @servers(['web' => ['root@139.59.23.89']])
+
+@task('deploy', ['on' => 'web'])
+	adduser poush
+	usermod -aG sudo poush
+@endtask
--- routes/web.php
+++ routes/web.php
@@ -14,36 +14,47 @@
 use DigitalOceanV2\Adapter\BuzzAdapter;
 use DigitalOceanV2\DigitalOceanV2;
 
-Route::get('/', function () {
-    // return view('welcome');
-    // $additionalProviderConfig = ['site' => 'meta.stackoverflow.com'];
-	
-	$config = new \SocialiteProviders\Manager\Config(
-				config('services.digitalocean.client_id'),
-				config('services.digitalocean.client_secret'),
-				config('services.digitalocean.redirect'),
-				['scoaspe' => 'read+write']
-			);
+Route::get('/', function() {
+	return view('welcome');
+});
 
+Route::get('/dologin', function () {
+	Auth::logout();
 	return Socialite::with('digitalocean')->scopes(['read write'])->redirect();
 });
 
-Route::get('/admin', function(){
+Route::get('/afterdo', function() {
+	$data = Socialite::driver('digitalocean')->user();
+	dd($data);	
+	$user = User::where('email', $data->email)->first();
 
-	$user = Socialite::driver('digitalocean')->stateless()->user();	
+	// if( is_null($user) ){
+	// 	$user = 
+	// }
 
-	var_dump($user);
-
-	// dd($user->token);
-
-	$adapter = new BuzzAdapter($user->token);
-
-	// create a digital ocean object with the previous adapter
-	$digitalocean = new DigitalOceanV2($adapter);
-
-	$droplet = $digitalocean->droplet();
-	$created = $droplet->create('the-name', 'nyc1', '512mb', 'ubuntu-14-04-x64');
-	$droplets = $droplet->getAll();
-	dd($droplets);
 
 });
+
+// Route::get('/admin', function(){
+
+// 	$user = Socialite::driver('digitalocean')->stateless()->user();	
+
+// 	var_dump($user);
+
+// 	// dd($user->token);
+
+// 	$adapter = new BuzzAdapter($user->token);
+
+// 	// create a digital ocean object with the previous adapter
+// 	$digitalocean = new DigitalOceanV2($adapter);
+
+// 	$droplet = $digitalocean->droplet();
+// 	$created = $droplet->create('the-name', 'nyc1', '512mb', 'ubuntu-14-04-x64');
+// 	$droplets = $droplet->getAll();
+// 	dd($droplets);
+
+// });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index');
