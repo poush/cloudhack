f0acad3fdd69e48217a31526027fac871e987aec
Subham banga <subhambanga26@gmail.com>
2016-11-26 13:46:27 +0530

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Deploy To DO</title>
+        <br>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
